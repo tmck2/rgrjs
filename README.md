bac98aa5c4b258906db0b34e4b54ca12f59c90ea
Anthony McKinney <anthonymckinney@Anthonys-MacBook-Pro.local>
2017-01-15 02:36:27 -0500

    Update README.md and remove commented code.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 *React*
 
 Goofing around with React, GraphQL, JSX, and ES6 xpiled by Babel.
+
+Don't forget to export MONGO_URL environment variable that points to your Mongo database. 
--- server.js
+++ server.js
@@ -20,11 +20,3 @@ MongoClient.connect(process.env.MONGO_URL, (err, database) => {
 
   app.listen(3000, () => console.log('Listening on port 3000'));
 });
-
-//app.get("/data/links", (req, res) => {
-//  db.collection("links").find({}).toArray((err, links) => {
-//    if (err) throw err;
-//
-//    res.json(links);
-//  });
-//});
