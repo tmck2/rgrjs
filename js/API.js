f68e44d84084a2d625f4630f02fc97033faefc80
Anthony McKinney <anthonymckinney@Anthonys-MacBook-Pro.local>
2017-01-15 01:45:21 -0500

    Integrate MongoDb through GraphQL

--- js/API.js
+++ js/API.js
@@ -4,9 +4,16 @@ import ServerActions from "./actions/ServerActions"
 let API = {
   fetchLinks() {
     console.log("1. In the API");
-    // Ajax request to read /data/links
-    get("/data/links").done(resp => {
-      ServerActions.receiveLinks(resp);
+    get("/graphql", {
+      query: `{
+        links {
+          _id,
+          title,
+          url
+        }
+      }`
+    }).done(resp => {
+      ServerActions.receiveLinks(resp.data.links);
     });
   }
 };
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 
 import {MongoClient} from 'mongodb';
 
@@ -11,13 +13,18 @@ MongoClient.connect(process.env.MONGO_URL, (err, database) => {
   if (err) throw err;
 
   db = database;
+  app.use('/graphql', GraphQLHTTP({
+    schema: schema(db),
+    graphiql: true
+  }));
+
   app.listen(3000, () => console.log('Listening on port 3000'));
 });
 
-app.get("/data/links", (req, res) => {
-  db.collection("links").find({}).toArray((err, links) => {
-    if (err) throw err;
-
-    res.json(links);
-  });
-});
+//app.get("/data/links", (req, res) => {
+//  db.collection("links").find({}).toArray((err, links) => {
+//    if (err) throw err;
+//
+//    res.json(links);
+//  });
+//});
